032f87c1e3cfc5d5c146f241e2f5bece64bba373
Sheng-Hung Weng <goodog008@gmail.com>
2019-07-08 10:50:57 +0800

    透過 document 取得網頁內容
    Field:
    	1. document.querySelector <Query html document with class:banner>
    	2. innerHTML method <Get html session>

--- js/mystyle.js
+++ js/mystyle.js
@@ -1,3 +1,7 @@
-window.onload=function{
-    window.alert("init complete");
+window.onload=function(){
+    window.alert(
+        /* innerHTML 可以取得或設定 HTML Code 中的元素
+            使用query selector 可抓取 css */
+        document.querySelector(".banner").innerHTML 
+    );
 };
